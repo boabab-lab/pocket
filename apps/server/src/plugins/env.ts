e8954c57f18fa9df156b62b70daa5036e5674c25
Une Nyide <une.nyide@gmail.com>
2025-06-26 13:24:30 +0200

    fix: lint warning

--- apps/server/src/plugins/env.ts
+++ apps/server/src/plugins/env.ts
@@ -1,7 +1,6 @@
 import fp from 'fastify-plugin';
 import fastifyEnv, { FastifyEnvOptions } from '@fastify/env';
 import envSchema from '../schemas/config.ts';
-import { currentENV } from '../utils.ts';
 
 export default fp<FastifyEnvOptions>(async (fastify) => {
   await fastify.register(fastifyEnv, {
