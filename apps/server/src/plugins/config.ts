ee9b581a715eed5143f080a6616b0ca680764e21
Une Nyide <une.nyide@gmail.com>
2025-06-29 15:26:34 +0200

    docs: add docs to plugins

--- apps/server/src/plugins/config.ts
+++ apps/server/src/plugins/config.ts
@@ -2,6 +2,12 @@ import fp from 'fastify-plugin';
 import fastifyEnv, { FastifyEnvOptions } from '@fastify/env';
 import envSchema from '../schemas/config.ts';
 
+/*
+ * This plugin connects the .env files for different environments
+ * to the fastify applicaion
+ * @see https://github.com/fastify/fastify-env
+ */
+
 export default fp<FastifyEnvOptions>(async (fastify) => {
   await fastify.register(fastifyEnv, {
     confKey: 'config',
