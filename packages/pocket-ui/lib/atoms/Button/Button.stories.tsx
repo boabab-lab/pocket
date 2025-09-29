b4707bdc239700a01c636eaaf35fb953b4c7d09b
Une Nyide <47525023+Unomars4@users.noreply.github.com>
2025-09-29 22:39:16 +0200

    feat: update button stories and variants

--- packages/pocket-ui/lib/atoms/Button/Button.stories.tsx
+++ packages/pocket-ui/lib/atoms/Button/Button.stories.tsx
@@ -12,14 +12,7 @@ const meta: Meta<typeof Button> = {
     },
     design: {
       control: 'select',
-      options: [
-        'outline_light',
-        'outline_dark',
-        'filled_light',
-        'filled_dark',
-        'filled_pink',
-        'neutral_dark',
-      ],
+      options: ['default', 'secondary'],
     },
   },
 };
@@ -27,71 +20,20 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const LargeOutlineDark: Story = {
+export const Default: Story = {
   args: {
-    label: 'A Text',
-    size: 'large',
-    design: 'outline_dark',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
-  },
-};
-export const LargeOutlineLight: Story = {
-  args: {
-    label: 'A Text',
-    size: 'large',
-    design: 'outline_light',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
-  },
-};
-
-export const MediumFilledLightWithIcon: Story = {
-  args: {
-    label: 'A Text dsfg gf sdgh sghsgdh h',
+    label: 'Button',
     size: 'medium',
-    design: 'filled_light',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
+    design: 'default',
+    disabled: false,
   },
 };
 
-export const MediumFilledPinkWithIcon: Story = {
+export const Secondary: Story = {
   args: {
-    label: 'A Text dsfg gf sdgh sghsgdh h',
+    label: 'Button',
     size: 'medium',
-    design: 'filled_pink',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
-  },
-};
-
-export const MediumFilledDarkWithIcon: Story = {
-  args: {
-    label: 'A Text dsfg gf sdgh sghsgdh h',
-    size: 'medium',
-    design: 'filled_dark',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
-  },
-};
-
-export const SmallNeutralDark: Story = {
-  args: {
-    label: 'A Text sdkfjhsdkfnjsid',
-    size: 'small',
-    design: 'neutral_dark',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
-  },
-};
-
-export const NeutralLight: Story = {
-  args: {
-    label: 'A Text sdkfjhsdkfnjsid',
-    size: 'small',
-    design: 'neutral_light',
-    icon: <CircleDashed className="w-4 h-4" />,
-    disabled: true,
+    design: 'secondary',
+    disabled: false,
   },
 };
--- packages/pocket-ui/lib/atoms/Button/Button.tsx
+++ packages/pocket-ui/lib/atoms/Button/Button.tsx
@@ -5,25 +5,13 @@ export const button = tv({
   base: 'inline-flex items-center max-w-[500px] hover:cursor-pointer',
   variants: {
     size: {
-      large: 'h-[44px] py-[12px] px-[16px] gap-1 rounded-lg',
-      medium: 'h-[32px] py-[6px] px-[12px] gap-1 rounded-lg',
-      small: 'h-[28px] py-[6px] px-[12px] gap-1 rounded-lg',
+      large: 'h-[44px] text-base py-[12px] px-[16px] gap-1 rounded-lg',
+      medium: 'h-[32px] text-sm py-[6px] px-[12px] gap-1 rounded-lg',
+      small: 'h-[28px] text-xs py-[6px] px-[12px] gap-1 rounded-lg',
     },
     design: {
-      outline_light:
-        'border border-greyscale-light-2 text-greyscale-light-2 hover:bg-greyscale-light-2 hover:text-greyscale-dark-1 hover:border-0',
-      outline_dark:
-        'border border-greyscale-dark-2 text-greyscale-dark-2 hover:bg-greyscale-dark-2 hover:text-greyscale-white hover:border-0',
-      filled_light:
-        'bg-greyscale-light-2 text-greyscale-dark-1 hover:bg-pink hover:text-greyscale-white',
-      filled_dark:
-        'bg-greyscale-dark-2 text-greyscale-white hover:bg-dark-pink hover:text-pink',
-      filled_pink:
-        'bg-pink text-greyscale-white hover:bg-dark-pink hover:text-pink',
-      neutral_dark:
-        'text-greyscale-dark-2 hover:bg-greyscale-light-2 hover:text-greyscale-dark-1',
-      neutral_light:
-        'text-greyscale-light-2 hover:bg-greyscale-light-2 hover:text-greyscale-dark-1',
+      default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+      secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/90'
     },
     disabled: {
       true: 'pointer-events-none',
@@ -33,32 +21,7 @@ export const button = tv({
     {
       design: ['filled_dark', 'filled_pink'],
       disabled: true,
-      class: 'bg-greyscale-light-1 text-greyscale-light-2',
-    },
-    {
-      design: 'filled_light',
-      disabled: true,
-      class: 'text-greyscale-medium-3 bg-greyscale-light-1',
-    },
-    {
-      design: 'outline_light',
-      disabled: true,
-      class: 'text-greyscale-medium-3 border-greyscale-medium-3',
-    },
-    {
-      design: 'outline_dark',
-      disabled: true,
-      class: 'text-greyscale-light-1 border-greyscale-light-1',
-    },
-    {
-      design: 'neutral_dark',
-      disabled: true,
-      class: 'text-greyscale-light-1',
-    },
-    {
-      design: 'neutral_light',
-      disabled: true,
-      class: 'text-greyscale-medium-3',
+      class: 'bg- text-',
     },
   ],
 });
@@ -74,8 +37,8 @@ export interface ButtonProps
 
 export const Button = ({
   label,
-  size,
-  design,
+  size = 'medium',
+  design = 'default',
   disabled = false,
   icon = undefined,
   ...props
